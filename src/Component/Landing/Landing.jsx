11ea559e3bdc701f42c51b694162c2ee88595c15
Mayur Mendhe <mayurmendhe28@gmail.com>
2022-05-15 23:05:19 +0530

    langing page is completed

--- src/Component/Landing/Landing.jsx
+++ src/Component/Landing/Landing.jsx
@@ -1,3 +1,17 @@
+import tshirt from "../../Assets/t-shirt.webp"
+import hells from "../../Assets/heels.webp"
+import beauty from "../../Assets/beauty.webp"
+import backpack from "../../Assets/backpacks.webp"
+import trousers from "../../Assets/trousers.webp"
+import kurta from "../../Assets/kurta.webp"
+import trackpants from "../../Assets/trackpants.webp"
+import home from "../../Assets/home-decor.webp"
+import dresses from "../../Assets/dresses.webp"
+import active from "../../Assets/activewear.jpg"
+
+
+
+
 
 import "./Landing.css"
 
@@ -11,6 +25,24 @@ export const Landing =()=>{
             <img src="https://images.pexels.com/photos/1049317/pexels-photo-1049317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="banner" />
             <h2>Extra Item @ 49 & more extra deals</h2>
             <h4 id="midnight"> 7 pm MIDNIGHT </h4>
+        </div>
+
+        <div>
+            <h1 id="walk"> Walk On Sunshine with these </h1>
+            <h1> BUDGET BUYS </h1>
+            <h1 id="viewAll"> +View All </h1>
+        </div>
+        <div className="gallery">
+            <img src={tshirt} alt="" />
+            <img src={hells} alt="" />
+            <img src={beauty} alt="" />
+            <img src={backpack } alt="" />
+            <img src={trousers} alt="" />
+            <img src={trackpants} alt="" />
+            <img src={home} alt="" />
+            <img src={dresses} alt="" />
+            <img src={active} alt="" />
+            <img src={kurta} alt="" />
         </div>
         
     </div>
