c32604d21913e8ad60c3777f74dd3a444cd33f6e
Mayur Mendhe <mayurmendhe28@gmail.com>
2022-05-22 15:14:49 +0530

    color filter added

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -1,40 +1,88 @@
-import "./Product.css"
-import {women} from "../../Assets/Women/women.js"
+import "./Product.css";
+import { women } from "../../Assets/Women/women.js";
+import { pink } from "@mui/material/colors";
+import Checkbox from "@mui/material/Checkbox";
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-
-
-export const Product = ()=>{
-
-
-    return <div id="container">
-
-       <div id="filters">
-       
-       </div>
-
-        <div className="products">
-
-
-            {
-                women.map((el)=>{
-                    
-                  return  <div className="gridDiv">
-
-                     <img className="images" src={el.image} alt={"image"} ></img>
-                     <h4 className="title"> {el.title} </h4>
-                     <h4 className="price" > Rs. {el.price} </h4>
-
-                    </div>
-
-                })
-            }
-
-
-
+export const Product = () => {
+  return (
+    <div id="container">
+      <div id="filters">
+        <div id="colors">
+          <div>
+            <Checkbox
+              {...label}
+              sx={{
+                color: pink[800],
+                "&.Mui-checked": {
+                  color: pink[600],
+                },
+              }}
+            />
+            Black
+          </div>
+          <div>
+            <Checkbox
+              {...label}
+              sx={{
+                color: pink[800],
+                "&.Mui-checked": {
+                  color: pink[600],
+                },
+              }}
+            />
+            White
+          </div> <div>
+            <Checkbox
+              {...label}
+              sx={{
+                color: pink[800],
+                "&.Mui-checked": {
+                  color: pink[600],
+                },
+              }}
+            />
+            Red
+          </div>
+           <div>
+            <Checkbox
+              {...label}
+              sx={{
+                color: pink[800],
+                "&.Mui-checked": {
+                  color: pink[600],
+                },
+              }}
+            />
+            Pink
+          </div>
+          <div>
+            <Checkbox
+              {...label}
+              sx={{
+                color: pink[800],
+                "&.Mui-checked": {
+                  color: pink[600],
+                },
+              }}
+            />
+            Red
+          </div>
+          
         </div>
+      </div>
 
-
-       
-
+      <div className="products">
+        {women.map((el, i) => {
+          return (
+            <div className="gridDiv" key={i}>
+              <img className="images" src={el.image} alt={"image"}></img>
+              <h4 className="title"> {el.title} </h4>
+              <h4 className="price"> Rs. {el.price} </h4>
+            </div>
+          );
+        })}
+      </div>
     </div>
-}
+  );
+};
